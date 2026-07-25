d765529845da6a8cec2f82d65713c34145aed280
mardin ajalah <tredingb25@gmail.com>
2026-07-25 20:58:24 +0700

    refactor: streamline route handler syntax for product routes

--- apps/backend/src/router/product.route.ts
+++ apps/backend/src/router/product.route.ts
@@ -12,17 +12,10 @@ const productRepository = new ProductRepository();
 const unitRepository = new UnitsRepository();
 const categoryRepository = new CategoryRepository();
 const productService = new ProductService(productRepository, unitRepository, categoryRepository);
-
 const productController = new ProductController(productService);
 
-router.get('/', (req, res) => {
-  productController.getProducts(req, res);
-});
-router.get('/:id', (req, res) => {
-  productController.getProductById(req, res);
-});
-router.post('/', (req, res) => {
-  productController.createProduct(req, res);
-});
+router.get('/', (req, res) => productController.getProducts(req, res));
+router.get('/:id', (req, res) => productController.getProductById(req, res));
+router.post('/', (req, res) => productController.createProduct(req, res));
 
 export default router;
