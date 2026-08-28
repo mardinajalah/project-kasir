3aa57794bf56972dddee524f706c7c5678c9421c
mardin ajalah <tredingb25@gmail.com>
2026-08-28 13:49:35 +0700

    feat: add category conytoller

---
+++ apps/backend/src/category/category.controller.ts
@@ -0,0 +1,28 @@
+import { CategoryType } from '@kasir/types';
+import type { Request, Response } from 'express';
+
+interface CategoryServiceType {
+  getAllCategories(): Promise<CategoryType[]>;
+  getCategoryById(categoryId: number): Promise<CategoryType>;
+  createCtegory(newCategory: CategoryType): Promise<CategoryType>;
+}
+
+export class CategoryController {
+  private categoryService;
+
+  constructor(categoryService: CategoryServiceType) {
+    this.categoryService = categoryService;
+  }
+
+  async getAllCategories(_req: Request, res: Response) {
+    try {
+      const dataCategories = await this.categoryService.getAllCategories()
+      res.status(200).json({
+        message: 'Categories fetched successfully',
+        data: dataCategories
+      })
+    } catch (error) {
+      res.status(500).json({ error: error instanceof Error ? error.message : 'Failed to fetch categories' });
+    }
+  }
+}
