897f27357524ab93983cca5060e5e44a28607291
mardin ajalah <tredingb25@gmail.com>
2026-08-30 13:15:39 +0700

    fix: change create pool database to promise create connection database

--- apps/backend/src/db/index.ts
+++ apps/backend/src/db/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 dotenv.config({ path: '.env.local' });
 
@@ -8,6 +8,6 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined in the environment variables.');
 }
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
-export const db = drizzle({ client: pool });
+export const db = drizzle({ client: connection });
